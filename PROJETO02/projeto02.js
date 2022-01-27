dd236bb65e47d785a30444fd47033f5335df863a
mvpbarbosa <mvpbarbosa16@gmail.com>
2022-01-27 19:03:36 -0300

    PROJETO02

--- PROJETO02/projeto02.js
+++ PROJETO02/projeto02.js
@@ -1,54 +1,128 @@
 console.clear();
 const prompt = require('prompt-sync')();
 
+console.log('Deseja jogar Jokenpô?');
+let continuar = prompt('Resposta: ');
+console.log('---------------------------------------------------');
 let movimentos = ['Pedra', 'Papel', 'Tesoura'];
-let escolha = prompt(`Pedra = 1 | Papel = 2 | Tesoura = 3 | Escolha seu movimento: `);
-let escolha2 = movimentos[escolha - 1]
-let escolhaAleatoria = Math.trunc(Math.random() * 3);
-let escolhaAleatoria2 = movimentos[escolhaAleatoria]
+console.log('Quantas rodadas você deseja jogar? ');
+let rodadas = +prompt('Resposta: ');
+console.log('---------------------------------------------------');
+let vitComp = 0;
+let vitJogador = 0;
 
-console.log('');
-console.log(`O jogador escolheu ${escolha2} e o computador escolheu ${escolhaAleatoria2}\n`)
+do 
+{
+    for (let contadorRodadas = 0; contadorRodadas < rodadas; contadorRodadas++)
+    {
+        console.log(`RODADA ${contadorRodadas + 1}`);
+        console.log();
+        console.log(`Pedra = 1 | Papel = 2 | Tesoura = 3`);
+        let escolha = +prompt('Escolha seu movimento: ');
+        let escolha2 = movimentos[escolha - 1];
+        let escolhaAleatoria = Math.trunc(Math.random() * 3);
+        let escolhaAleatoria2 = movimentos[escolhaAleatoria]
+        console.log();
 
-if (escolha2 == escolhaAleatoria2) 
-{
-    console.log('Empate! Fim de jogo.');
-} 
-else if (escolha2 == movimentos[0])
-{
-    if (escolhaAleatoria2 == movimentos[1]) 
+        while (escolha < 1 || escolha > 3)
+        {   
+            console.log('Resposta inválida');
+            console.log('---------------------------------------------------');
+            console.log(`RODADA ${contadorRodadas + 1}`);
+            console.log();
+            console.log(`Pedra = 1 | Papel = 2 | Tesoura = 3`);
+            escolha = +prompt('Escolha seu movimento: ');
+
+            if (escolha == 1 || escolha == 2 || escolha == 3)
+            {
+                break;
+            }
+
+        }
+        console.log(`O jogador escolheu ${escolha2} e o computador escolheu ${escolhaAleatoria2}`);
+        console.log('---------------------------------------------------');
+        console.log();
+
+        if (escolha2 == escolhaAleatoria2)
         {
-            console.log('O computador venceu a partida.');
+            console.log('Empate! O jogador e o computador recebem pontos. ');
+            vitComp++
+            vitJogador++
+            console.log();
+        
         } 
-        else if (escolhaAleatoria2 == movimentos[2])
+        else if (escolha2 == movimentos[0])
         {
-            console.log('O jogador venceu a partida.');
-        }    
-} 
-else if (escolha2 == movimentos[1])
-{
-    if (escolhaAleatoria2 == movimentos[2])
-    {
-        console.log('O computador venceu a partida.');
-    } 
-    else if (escolhaAleatoria2 == movimentos[0])
-    {
-        console.log('O jogador venceu a partida.');
-    }
-} 
-else 
-{
-    if (escolhaAleatoria2 == movimentos[1])
-    {
-        console.log('O computador venceu a partida.');
-    }
-    else if (escolhaAleatoria2 == movimentos[0])
-    {
-        console.log('O jogador venceu a partida.');
-    }
-}
+            if (escolhaAleatoria2 == movimentos[1])
+            {
+                console.log('O computador venceu a rodada!');
+                vitComp++
+                console.log();
 
+            }
+            else if (escolhaAleatoria2 == movimentos[2])
+            {
+                console.log('O jogador venceu a rodada!')
+                vitJogador++
+                console.log();
 
-console.log(escolha2);
-console.log(escolhaAleatoria2);
+            }
+        }
+        else if (escolha2 == movimentos[1])
+        {
+            if (escolhaAleatoria2 == movimentos [2])
+            {
+                console.log('O computador venceu a rodada!');
+                vitComp++
+                console.log();
+
+            }
+            else if (escolhaAleatoria2 == movimentos[0])
+            {
+                console.log('O jogador venceu a rodada!');
+                vitJogador++
+                console.log();
+
+            }
+        }
+        else 
+        {
+            if (escolhaAleatoria2 == movimentos[0])
+            {
+                console.log('O computador venceu a rodada!');
+                vitComp++
+                console.log();
+
+            }
+            else if (escolhaAleatoria2 == movimentos[1])
+            {
+                console.log('O jogador venceu a rodada!'); 
+                vitJogador++
+                console.log();
+
+            }
+        }
+        console.log(`JOGADOR ${vitJogador} X ${vitComp} COMPUTADOR`);
+        console.log('---------------------------------------------------');
+        console.log();
+    }
+    if (vitJogador == vitComp)
+    {
+        console.log('O jogo terminou em empate!');
+    }
+    else if (vitComp > vitJogador)
+    {
+        console.log('O Grande Campeão foi o Computador!');
+    }
+    else if (vitJogador > vitComp)
+    {
+        console.log('O Grande Campeão foi o Jogador!');
+    }
+        console.log('---------------------------------------------------');
+        console.log('Deseja continuar jogando?');
+        continuar = prompt('Resposta: ');
+        console.log();
+} 
+while (continuar == 's')
 console.log();
+
